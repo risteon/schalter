82393e6b755b9641ef1992cc17327209868bcaa7
Christoph Rist <c.rist@posteo.de>
2020-01-23 20:50:09 +0100

    Add package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='schalter',
+      version='0.1.0',
+      description='Fast and simple configuration',
+      url='http://github.com/risteon/schalter',
+      author='Christoph Rist',
+      author_email='c.rist@posteo.de',
+      license='Apache-2.0',
+      packages=['schalter'],
+      zip_safe=False,
+      install_requires=[
+          'ruamel.yaml',
+      ],
+      setup_requires=['pytest-runner']
+      tests_require=['pytest'],
+      python_requires='>=3.5',
+      )
---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Christoph Rist"""
+__email__ = 'c.rist@posteo.de'
+
